24a966aba59944f8c7fb1dca2bb5547adf458847
AxelCipher20 <evasiveride5782@proton.me>
2026-02-26 00:02:38 +0900

    いくつかの問題を修正

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,11 @@
 ﻿import type { Config } from "tailwindcss";
-import { wedgesTW } from "@lemonsqueezy/wedges";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@lemonsqueezy/wedges/dist/esm/index.js",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [wedgesTW()],
 };
 
 export default config;
